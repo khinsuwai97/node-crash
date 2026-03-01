299ba6132efeeea448d704cedb29999ac39b8813
khinsuwai97 <khinsuwai009@gmail.com>
2026-03-01 13:55:09 +0700

    test node js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ import http from 'http';
 
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.statusCode = 404;
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 404;
   // res.write('<h1>Hello World</h1>');
-  res.end('<h1>Hello World</h1>');
+  res.writeHead(500, { 'Content-Type': 'text/html' });
+  res.end(JSON.stringify({ message: 'Internal Server Error' }));
 });
 
 server.listen(PORT, () => {
